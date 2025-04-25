2933623e19d4ea914b8d0786c2b505abb89d4bdc
Yuyeseul <ryuys0308@naver.com>
2025-04-25 18:01:45 +0900

    회원 기능 테스트2

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -5,7 +5,10 @@ import jpabook.jpashop.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -45,5 +48,26 @@ class MemberServiceTest {
 
         // then
         assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    }
+
+    @Test
+    public void 회원_전체_조회() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("yeseul");
+
+        Member member2 = new Member();
+        member2.setName("yu");
+
+        memberService.join(member1);
+        memberService.join(member2);
+
+        // when
+        List<Member> members = memberService.findMembers();
+
+        // then
+        assertEquals(2, members.size());
+        assertTrue(members.contains(member1));
+        assertTrue(members.contains(member2));
     }
 }
