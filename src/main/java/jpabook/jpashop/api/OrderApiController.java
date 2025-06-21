0ce1aeed88728aebeb01ec3c33f72d231f4b4fb1
Yuyeseul <ryuys0308@naver.com>
2025-06-21 20:14:51 +0900

    API 개발 고급 - 컬렉션 조회 최적화
    - 주문 조회 V6 : JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
@@ -81,6 +83,19 @@ public class OrderApiController {
     @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> ordersV5() {
         return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    // 주문 조회 V6 : JPA에서 DTO로 직접 조회, 플랫 데이터 최적화
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
     }
 
     @Data
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -1,14 +1,15 @@
 package jpabook.jpashop.repository.order.query;
 
-import jpabook.jpashop.api.OrderApiController;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
 
     private Long orderId;                       // 주문 id
@@ -16,7 +17,7 @@ public class OrderQueryDto {
     private LocalDateTime orderDate;            // 주문시간
     private OrderStatus orderStatus;            // 주문상태
     private Address address;                    // 배송지 정보
-    private List<OrderItemQueryDto> orderItems;  // 주문상품정보
+    private List<OrderItemQueryDto> orderItems; // 주문상품정보
 
     public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
         this.orderId = orderId;
@@ -24,5 +25,14 @@ public class OrderQueryDto {
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
